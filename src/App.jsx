25902806480a48f7fe8090bf3404e0aafdddb754
Santiago Robledo <santiddf9@gmail.com>
2025-08-19 16:29:06 -0300

    agregue un componente para dar de alta peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-function App() {
-  const [count, setCount] = useState(0)
+import AltaMovie from "./components/AltaMovie";
 
+function App() {
   return (
     <main>
-
+      <AltaMovie />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AltaMovie.jsx
@@ -0,0 +1,47 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const AltaMovie = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center my-5">Ingresa una nueva película</h1>
+      <Form className="border p-4 rounded-4 shadow">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Ingresa un nombre" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicSelect">
+          <Form.Label>Género</Form.Label>
+          <Form.Select as="select" required>
+            <option>Comedia</option>
+            <option>Acción</option>
+            <option>Infantil</option>
+            <option>Terror</option>
+            <option>Drama</option>
+            <option>Romance</option>
+            <option>Ciencia Ficción</option>
+            <option>Documental</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Descripción</Form.Label>
+          <Form.Control
+            required
+            style={{ resize: "none" }}
+            as="textarea"
+            rows={3}
+            placeholder="Ingresa una descripción"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Agregar
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AltaMovie;
