c9e93157a800637675b3a476bb0fcf86d330f1ef
Santiago Robledo <santiddf9@gmail.com>
2025-08-19 17:59:45 -0300

    añadi la logica para crear una nueva pelicula

--- src/components/AltaMovie.jsx
+++ src/components/AltaMovie.jsx
@@ -1,30 +1,49 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
+import Movie from "../MovieClass";
 
 const AltaMovie = () => {
-  //guardar los datos de la película
-  const [movie, setMovie] = useState({
-    name: "",
-    genero: "",
-    descripcion: "",
-  });
-  // guardar los datos en el array pusheando
+  const [movies, setMovies] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [genero, setGenero] = useState("");
+  const [descripcion, setDescripcion] = useState("");
 
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    const nuevaPelicula = new Movie(nombre, genero, descripcion);
+    setMovies([...movies, nuevaPelicula]);
+    const updatedMovies = [...movies, nuevaPelicula];
+    //limpia los campos
+    setNombre("");
+    setGenero("");
+    setDescripcion("");
+    console.log(updatedMovies);
+  }
 
   return (
     <div className="container">
       <h1 className="text-center my-5">Ingresa una nueva película</h1>
-      <Form className="border p-4 rounded-4 shadow">
+      <Form className="border p-4 rounded-4 shadow" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingresa un nombre" required />
+          <Form.Control
+            type="text"
+            placeholder="Ingresa un nombre"
+            required
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicSelect">
           <Form.Label>Género</Form.Label>
-          <Form.Select as="select" required>
+          <Form.Select
+            required
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+          >
+            <option value="">Selecciona un género</option>
             <option>Comedia</option>
             <option>Acción</option>
             <option>Infantil</option>
@@ -44,6 +63,8 @@ const AltaMovie = () => {
             as="textarea"
             rows={3}
             placeholder="Ingresa una descripción"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
           />
         </Form.Group>
 
